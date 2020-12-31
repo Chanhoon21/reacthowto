9f0ba7816b31b0161b7d783277e295aefb359741
ChanHoon <ribon3594@gmail.com>
2021-01-01 01:47:24 +0900

    2021/1/1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+
+  render(){
+    return(
+
+      <div className="App">
+        <img src="https://img.etoday.co.kr/pto_db/2020/06/20200620001049_1474738_467_625.jpg"></img>
+
+      </div>
+    )
+
+
+  }
+}
+
+export default App;
