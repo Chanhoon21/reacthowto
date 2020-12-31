fd2e38169e741c23f0cd8315581c59c0fc6efb58
ChanHoon <ribon3594@gmail.com>
2021-01-01 01:49:56 +0900

    1/1일 커밋

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component{
     return(
 
       <div className="App">
-        <img src="https://img.etoday.co.kr/pto_db/2020/06/20200620001049_1474738_467_625.jpg"></img>
+        <img src="https://scontent-ssn1-1.xx.fbcdn.net/v/t1.0-9/46463894_2246523795584724_5340350125254377472_o.jpg?_nc_cat=106&ccb=2&_nc_sid=8bfeb9&_nc_ohc=3InN3ZaL4EkAX8DG_R3&_nc_ht=scontent-ssn1-1.xx&oh=f6828a5fc9b921453e4cb5edef7a1608&oe=6013413E"></img>
 
       </div>
     )
